1e61e279aaaad2898bd76ee125447043590f1fb9
humzauddin27 <humzauddin7@gmail.com>
2017-09-13 16:35:23 -0400

    initial commit

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { Text, View, Image, Linking } from 'react-native'
+import Card from './Card'
+import CardSection from './CardSection'
+import Button from './Button'
+
+const AlbumDetail = ({ album }) => { 
+	const { title, artist, thumbnail_image, image, url } = album;
+	const { headerContentStyle,
+			imageStyle,
+			thumbnailStyle,
+			headerTextStyle,
+			albumCoverStyle	} = styles;
+	return (
+		<Card>
+
+			<CardSection>
+				<View style={thumbnailStyle}>
+					<Image
+						style={imageStyle}
+						source={{ uri: thumbnail_image }}
+					/>
+				</View>
+				<View style={headerContentStyle}> 
+					<Text style={headerTextStyle}> {title} </Text>
+					<Text> {artist} </Text>
+				</View>
+			</CardSection>
+
+			<CardSection>
+				<Image source={ {uri: image} } style={albumCoverStyle} />
+			</CardSection>
+
+			<CardSection>
+				<Button onPress={() => Linking.openURL(url)}>
+					Buy Now
+				</Button>
+			</CardSection>
+
+		</Card>
+	)
+};
+
+let styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	headerTextStyle: {
+		fontSize: 18
+	},
+	imageStyle: {
+		height: 50,
+		width: 50
+	},
+	thumbnailStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	},
+	albumCoverStyle: {
+		height: 300,
+		flex: 1,
+		width: null
+	}
+};
+
+export default AlbumDetail;
